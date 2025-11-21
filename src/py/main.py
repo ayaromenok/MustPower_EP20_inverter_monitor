f8ba5e7041de8cdb334ca541a23d0b8a7243545e
Andrey Yaromenok <ayaromenok@gmail.com>
2025-11-21 15:47:22 +0200

    up: comment some debug info - leave only status updates

--- src/py/main.py
+++ src/py/main.py
@@ -7,7 +7,7 @@ import paho.mqtt.client as mqtt
 
 
 def init_all():
-    print("init_all()")
+#    print("init_all()")
     data0 = b"\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
     format_string = '>BHHHHHHHHHHHHHHHHHHHHHHHHHHHI'
     ups_data = struct.unpack(format_string, data0)
@@ -38,7 +38,7 @@ def read_serial_data_EP20(ups_data, port_name):
 
             if data1:
                 data1_size = sys.getsizeof(data1)
-                print(f"Received {data1_size} bytes, data:{data1.hex()}") # Print as hex string
+#                print(f"Received {data1_size} bytes, data:{data1.hex()}") # Print as hex string
                 # B - uint8, H - uint16, I - uint32, Q - uint64
                 format_string1 = '>BHHHHHHHHHHHHHHHHHHHHHHHHHHHI'
 
@@ -47,22 +47,22 @@ def read_serial_data_EP20(ups_data, port_name):
                 if (ups_data[20] == 0) & (ups_data[0] != 1): #sometime all zeros came from port
                     print ("use old values")
                     ups_data = old_ups_data
-                if ups_data[17] >= (300+100): # when battery discarged - value 65468 appear instead of 0#; 300 is 30Amps - maximus possible value fro Ep20 series
-                     print ("charge current is 65468, need to fix to 0")
+#                if ups_data[17] >= (300+100): # when battery discarged - value 65468 appear instead of 0#; 300 is 30Amps - maximus possible value fro Ep20 series
+#                     print ("charge current is 65468, need to fix to 0")
 # WtF wrong with next line?
 #                     ups_data[17] = 0
 
-                print(f"Grid Voltage:     {(ups_data[7]*0.1):.1f} V")
-                print(f"Grid Frequence:   {(ups_data[8]*0.1):.1f} Hz")
-                print(f"Output Voltage:   {(ups_data[9]*0.1):.1f} V")
-                print(f"Output Frequency: {(ups_data[10]*0.1):.1f} Hz")
-                print(f"Battery Voltage:  {ups_data[16]*.1:.1f} V")
-                print(f"Bat Charge Curr:  {ups_data[17]*.1:.1f} A")
-                print(f"Bat Load Currrnt: {ups_data[11]} A")
-                print(f"Load Power:       {ups_data[12]} W")
-                print(f"Load Percent:     {ups_data[14]} %")
-                print(f"Battery SOC:      {ups_data[19]} %")
-                print(f"Transform Temp:   {ups_data[20]} C")
+#                print(f"Grid Voltage:     {(ups_data[7]*0.1):.1f} V")
+#                print(f"Grid Frequence:   {(ups_data[8]*0.1):.1f} Hz")
+#                print(f"Output Voltage:   {(ups_data[9]*0.1):.1f} V")
+#                print(f"Output Frequency: {(ups_data[10]*0.1):.1f} Hz")
+#                print(f"Battery Voltage:  {ups_data[16]*.1:.1f} V")
+#                print(f"Bat Charge Curr:  {ups_data[17]*.1:.1f} A")
+#                print(f"Bat Load Currrnt: {ups_data[11]} A")
+#                print(f"Load Power:       {ups_data[12]} W")
+#                print(f"Load Percent:     {ups_data[14]} %")
+#                print(f"Battery SOC:      {ups_data[19]} %")
+#                print(f"Transform Temp:   {ups_data[20]} C")
 
             else:
                 print("No data received.")
@@ -108,5 +108,4 @@ def publish_message(port_name, ups_data, time_interval):
 if __name__ == "__main__":
     time_interval = 5; #sec, for debugging
     ups_data = init_all();
-    print (ups_data);
     publish_message('/dev/ttyUSB0',ups_data, time_interval)
